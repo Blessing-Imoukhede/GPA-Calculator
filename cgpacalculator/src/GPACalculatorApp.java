12810d13691ff02356c1d18d6fa14667a26ccef5
Blessing Imoukhede <153104979+Blessing-Imoukhede@users.noreply.github.com>
2023-12-11 07:36:16 +0100

    Update GPACalculatorApp.java

--- cgpacalculator/src/GPACalculatorApp.java
+++ cgpacalculator/src/GPACalculatorApp.java
@@ -10,15 +10,46 @@ public class GPACalculatorApp {
         while (true) {
             System.out.print("Enter course code (or 'exit' to finish): ");
             String courseCode = scanner.nextLine();
+
             if (courseCode.equalsIgnoreCase("exit")) {
                 break;
             }
 
-            System.out.print("Enter course unit: ");
-            int courseUnit = scanner.nextInt();
-            System.out.print("Enter course score: ");
-            int courseScore = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
+            // Input validation for course unit
+            int courseUnit;
+            while (true) {
+                System.out.print("Enter course unit: ");
+                if (scanner.hasNextInt()) {
+                    courseUnit = scanner.nextInt();
+                    scanner.nextLine(); // Consume the newline character
+                    if (courseUnit >= 0) {
+                        break; // Valid input, exit the loop
+                    } else {
+                        System.out.println("Invalid input. Please enter a non-negative integer for course unit.");
+                    }
+                } else {
+                    System.out.println("Invalid input. Please enter a valid integer for course unit.");
+                    scanner.nextLine(); // Consume the invalid input
+                }
+            }
+
+            // Input validation for course score
+            int courseScore;
+            while (true) {
+                System.out.print("Enter course score: ");
+                if (scanner.hasNextInt()) {
+                    courseScore = scanner.nextInt();
+                    scanner.nextLine(); // Consume the newline character
+                    if (courseScore >= 0 && courseScore <= 100) {
+                        break; // Valid input, exit the loop
+                    } else {
+                        System.out.println("Invalid input. Please enter a score between 0 and 100.");
+                    }
+                } else {
+                    System.out.println("Invalid input. Please enter a valid integer for course score.");
+                    scanner.nextLine(); // Consume the invalid input
+                }
+            }
 
             Course course = new Course(courseCode, courseUnit, courseScore);
             courses.add(course);
@@ -30,7 +61,6 @@ public class GPACalculatorApp {
 
         for (Course course : courses) {
             PrintUtility.printCourseInfo(course);
-
             totalGradeUnit += course.getUnit() * course.calculateQualityPoint();
             totalCourseUnit += course.getUnit();
         }
